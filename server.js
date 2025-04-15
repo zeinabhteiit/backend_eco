b6a26c1b8faf2028275173c91f0a8220fbd3935a
zeinab <zeinabhteit1@gmail.com>
2025-04-15 13:22:01 +0300

    feat(ordershipmentaddress): implement CRUD operations

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ import brandRoutes from "./src/routes/brandRoutes.js";
 import orderRoutes from "./src/routes/orderRoutes.js"; 
 import reviewRoutes from "./src/routes/reviewRoutes.js";
 import orderShipmentRoutes from './src/routes/ordershipmentRoutes.js';
+import orderShipmentAddressRoutes from './src/routes/ordershipmentaddressRoutes.js';
+
 
 dotenv.config();
 const app = express();
@@ -23,6 +25,8 @@ app.use("/api/brands", brandRoutes); // Prefix for brand routes
 app.use("/api/orders", orderRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/ordershipments", orderShipmentRoutes);
+app.use("/api/ordershipmentaddress", orderShipmentAddressRoutes);
+
 
 // Server
 const PORT = process.env.PORT || 5000;
